da4c98e3876c10cf52aa51ece3b62c5e8b8e335a
Vivian Fang <v.fang@berkeley.edu>
2021-08-20 17:41:04 +0000

    add more config files to provision machines

--- scripts/provision_machines.sh
+++ scripts/provision_machines.sh
@@ -6,9 +6,12 @@ python3 runExperiment.py -p -f 9
 python3 update_config.py -m="config/machines.json" config/adaptiveScaleThroughput_300.json
 python3 update_config.py -m="config/machines.json" config/adaptiveScaleThroughput_500.json
 python3 update_config.py -m="config/machines.json" config/adaptiveScaleThroughputOblix.json
+python3 update_config.py -m="config/machines.json" config/adaptiveScaleThroughputOblix_500.json
+python3 update_config.py -m="config/machines.json" config/adaptiveScaleThroughputOblix_300.json
 python3 update_config.py -m="config/machines.json" config/scaleData.json
 python3 update_config.py -m="config/machines.json" config/scaleDataLatency.json
 python3 update_config.py -m="config/machines.json" config/balancerMakeBatch.json
 python3 update_config.py -m="config/machines.json" config/balancerMatchResponses.json
 python3 update_config.py -m="config/machines.json" config/suboramSort.json
+python3 update_config.py -m="config/machines.json" config/suboramProcessBatch.json
 python3 update_config.py -m="config/machines.json" config/suboramProcessBatchThreads.json
